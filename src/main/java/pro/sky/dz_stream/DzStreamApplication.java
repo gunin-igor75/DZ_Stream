9506460bf4f7641bee9c57c02c515871539d558f
Igor Gunin <gunin-igor75@yandex.ru>
2022-09-29 08:29:33 +0600

    First commit

---
+++ src/main/java/pro/sky/dz_stream/DzStreamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.dz_stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DzStreamApplication.class, args);
+    }
+
+}
